99224fe145e6c1023bb733eb9ecf9c6898995e99
qcon <marvinmieth@icloud.com>
2015-08-02 10:31:10 +0200

    gulp

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer')
+
+gulp.task('autoprefixer', function() {
+	return gulp.src('www/css/main.css')
+		.pipe(autoprefixer({
+			browsers: ['last 2 versions']
+		}))
+		.pipe(gulp.dest('www/css/'))
+});
+
+gulp.task('sass', function() {
+	gulp.src('./src/sass/*.scss')
+	.pipe(sass({
+		outputStyle: 'compressed'
+	}).on('error', sass.logError))
+	.pipe(gulp.dest('./www/css'))
+});
